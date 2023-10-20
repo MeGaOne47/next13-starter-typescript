1d5a56747f173f52abcaf335661c9622a3d3685b
TanHung <nguyentanhung9a1@gmail.com>
2023-10-20 16:11:43 +0700

    Hoàn thành chức năng view detail blogs

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -1,10 +1,33 @@
 'use client'
-
 import {useRouter} from "next/navigation";
 import {Button, Card} from "react-bootstrap";
+import useSWR, { Fetcher } from 'swr'
 
 const ViewDetailBlog = ({ params }: { params: { id: string } }) => {
 
+
+
+    const fetcher: Fetcher<IBlog, string> = (url: string) => fetch(url)
+        .then((res) => res.json());
+
+    const {data, error, isLoading} = useSWR(
+        `http://localhost:8000/blogs/${params.id}`,
+        fetcher,
+        {
+            revalidateIfStale: false,
+            revalidateOnFocus: false,
+            revalidateOnReconnect: false
+        }
+    );
+    if (isLoading) {
+        return (
+            <div>
+                Loading...
+            </div>
+        )
+    }
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
     const router = useRouter()
     const handleBtn = () => {
         // alert(" Trở về trang chủ");
@@ -22,15 +45,15 @@ const ViewDetailBlog = ({ params }: { params: { id: string } }) => {
             Vieu detail with hoidanit = {params.id}
             <div>
                 <Card className="text-center">
-                    <Card.Header>Title</Card.Header>
+                    <Card.Header>{data?.title}</Card.Header>
                     <Card.Body>
-                        <Card.Title>Special title treatment</Card.Title>
+                        {/* <Card.Title>Special title treatment</Card.Title> */}
                         <Card.Text>
-                            With supporting text below as a natural lead-in to additional content.
+                            {data?.content}
                         </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
+                        {/* <Button variant="primary">Go somewhere</Button> */}
                     </Card.Body>
-                    <Card.Footer className="text-muted">Author</Card.Footer>
+                    <Card.Footer className="text-muted">Author: {data?.author}</Card.Footer>
                 </Card>
             </div>
             <div style={{ textAlign: 'center' }}>
